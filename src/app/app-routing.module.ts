c6ff141f07769b4538733318db0356dbf01212f6
QuinntyneBrown <quinntynebrown@gmail.com>
2021-05-09 12:10:42 -0400

    feat(navbar): initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HorizontalNavBarComponent } from './horizontal-nav-bar/horizontal-nav-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: NavBarComponent
+  },
+  {
+    path: "horizontal-nav-bar", component: HorizontalNavBarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HorizontalNavBarComponent } from './horizontal-nav-bar/horizontal-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HorizontalNavBarComponent
   ],
   imports: [
     BrowserModule,
